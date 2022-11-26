5a286d8171e5b7700bf7d5d99ae8d41f49a59e8d
Luis <cr7rg4l1999@gmail.com>
2022-11-26 18:34:30 -0500

    Commit 4

--- lib/routes/my_routes.dart
+++ lib/routes/my_routes.dart
@@ -5,19 +5,14 @@ import 'package:MiAlcaldia/pages/home_page.dart';
 import 'package:MiAlcaldia/pages/login_page.dart';
 import 'package:MiAlcaldia/pages/register_page.dart';
 import 'package:MiAlcaldia/pages/regiter_login_page.dart';
+import 'package:MiAlcaldia/ui/listview_product.dart';
 import 'package:get/route_manager.dart';
-
-
 
 routes() => [
       GetPage(name: "/home", page: () => LoginPage()),
       GetPage(name: "/registration", page: () => RegisterPage()),
       GetPage(name: "/loginpage", page: () => LoginPage()),
       GetPage(name: "/logingoogle", page: () => LoginPage()),
-      GetPage(
-          name: "/principalpage",
-          page: () => InterfazHome()),
-          GetPage(
-          name: "/principalpage2",
-          page: () => InterfazHomeEmail()),
+      GetPage(name: "/principalpage", page: () => ListViewProduct()),
+      GetPage(name: "/principalpage2", page: () => InterfazHomeEmail()),
     ];
